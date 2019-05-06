be6469fabe6096e028a63f0c030ffaa6150ba889
msuleman91 <msuleman387@gmail.com>
2019-05-06 14:44:53 -0700

    sec commit

--- src/calculator/JavaCalculator.java
+++ src/calculator/JavaCalculator.java
@@ -2,15 +2,15 @@ package calculator;
 
 public class JavaCalculator {
 	
-	public void sum(int a, int b) {
+	public int sum(int a, int b) {
 		
-		System.out.println(a+b);
+		return a+b;
 	}
 	
 	public static void main(String[] args) {
 		
 		JavaCalculator cal= new JavaCalculator();
-		cal.sum(10, 1);
+		System.out.println(cal.sum(10, 1));
 	}
 	
 
