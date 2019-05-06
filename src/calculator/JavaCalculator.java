3edbcc251d1df0c7872b1c8a59fcc94451b25a22
msuleman91 <msuleman387@gmail.com>
2019-05-06 14:48:46 -0700

    3rd commit

--- src/calculator/JavaCalculator.java
+++ src/calculator/JavaCalculator.java
@@ -7,10 +7,17 @@ public class JavaCalculator {
 		return a+b;
 	}
 	
+	public int sub(int a, int b) {
+		
+		return a-b;
+	}
+	
 	public static void main(String[] args) {
 		
 		JavaCalculator cal= new JavaCalculator();
 		System.out.println(cal.sum(10, 1));
+		
+		System.out.println(cal.sub(10,2));
 	}
 	
 
