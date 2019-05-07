a40c4ca327a87e1240850982ccdf98788d093a16
msuleman91 <msuleman387@gmail.com>
2019-05-06 19:33:11 -0700

    1st commit from featurebranch

--- src/calculator/JavaCalculator.java
+++ src/calculator/JavaCalculator.java
@@ -1,24 +1,29 @@
 package calculator;
 
 public class JavaCalculator {
-	
+
 	public int sum(int a, int b) {
-		
-		return a+b;
+
+		return a + b;
 	}
-	
+
 	public int sub(int a, int b) {
-		
-		return a-b;
+
+		return a - b;
 	}
-	
+
+	public int mul(int a, int b) {
+
+		return a * b;
+	}
+
 	public static void main(String[] args) {
-		
-		JavaCalculator cal= new JavaCalculator();
+
+		JavaCalculator cal = new JavaCalculator();
 		System.out.println(cal.sum(10, 1));
-		
-		System.out.println(cal.sub(10,2));
+
+		System.out.println(cal.sub(10, 2));
+		System.out.println(cal.mul(10, 2));
 	}
-	
 
 }
